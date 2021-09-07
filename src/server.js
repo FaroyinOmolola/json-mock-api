764fa5df0bf91ccc53deab252d9ee9110828e105
seunexplicit <seunxplicit@gmail>
2021-09-07 21:37:19 +0100

    .

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+server.listen(server_port, server_host, () => {
   console.log("JSON Server is running");
 });
